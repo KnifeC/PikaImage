c5a1062e74f2ef987b86b1da4080bf88d8e2eef9
KnifeC <keshanechen@gmail.com>
2019-11-19 00:50:48 +0800

    finish login

--- src/main/java/moe/keshane/PikaImage/Config/LoginIntercepter.java
+++ src/main/java/moe/keshane/PikaImage/Config/LoginIntercepter.java
@@ -1,15 +1,43 @@
 package moe.keshane.PikaImage.Config;
 
+import moe.keshane.PikaImage.Common.KeySet;
+import moe.keshane.PikaImage.Dao.Entity.User;
+import moe.keshane.PikaImage.Service.UserService;
+import moe.keshane.PikaImage.Util.CookieUtils;
+import moe.keshane.PikaImage.Util.SessionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.security.Key;
 
 public class LoginIntercepter implements HandlerInterceptor {
+
+    @Autowired
+    UserService userService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return false;
+        String userId = CookieUtils.getRequestCookie(request, KeySet.UUID);
+        if(userId==null) {
+            request.setAttribute(KeySet.MESSAGE,"该操作需要登陆");
+            response.sendRedirect("/login");
+            return false;
+        }
+        User user = userService.getUserById(userId);
+        if(user == null){
+            request.setAttribute(KeySet.MESSAGE,"用户名或密码错误");
+            response.sendRedirect("/login");
+            return false;
+        }
+        HttpSession session = request.getSession();
+        SessionUtils.setSessionData(session,KeySet.USERID,user.getUserId());
+        SessionUtils.setSessionData(session,KeySet.USERNAME,user.getUsername());
+        SessionUtils.setSessionData(session,KeySet.USERTYPE,user.getType());
+        return true;
     }
 
     @Override
--- src/main/java/moe/keshane/PikaImage/Config/WebMvcConfig.java
+++ src/main/java/moe/keshane/PikaImage/Config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package moe.keshane.PikaImage.Config;
 
 import moe.keshane.PikaImage.Util.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
+        registry.addInterceptor(new LoginIntercepter()).addPathPatterns("/**").excludePathPatterns("/login","/register","/image/**");
     }
 
     @Override
