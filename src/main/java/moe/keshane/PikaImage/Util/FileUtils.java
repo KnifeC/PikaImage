6ad0626d07157945bbf4f0ceadb37bed169ab873
KnifeC <keshanechen@gmail.com>
2019-11-22 17:30:46 +0800

    update pages finish 'delete' 'copy' 'move' method to FileUtils add create delete method to DirectoryController

--- src/main/java/moe/keshane/PikaImage/Util/FileUtils.java
+++ src/main/java/moe/keshane/PikaImage/Util/FileUtils.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import moe.keshane.PikaImage.Common.FileKey;
 import moe.keshane.PikaImage.Exception.DirectoryAleadyExistException;
 import org.springframework.boot.system.ApplicationHome;
+import org.springframework.util.FileSystemUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +18,30 @@ import java.util.stream.Collectors;
 
 @Slf4j
 public class FileUtils {
+
+    public static boolean movaDirectory(String source,String target){
+        boolean b = copyDirectory(source,target);
+        if(b){
+            return deleteDirectory(source);
+        }
+        return false;
+    }
+
+    public static boolean copyDirectory(String source,String target){
+        try {
+            FileSystemUtils.copyRecursively(Paths.get(source),Paths.get(target));
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean deleteDirectory(String dirPath){
+        log.info("获得删除路径: {} ",dirPath);
+        boolean b = FileSystemUtils.deleteRecursively(Paths.get(dirPath).toFile());
+        return b;
+    }
 
     public static boolean isExist(String dirPath){
         if(!Files.exists(Paths.get(dirPath),new LinkOption[]{ LinkOption.NOFOLLOW_LINKS})){
@@ -49,7 +74,7 @@ public class FileUtils {
 
     public static String getUserRootPath(String userName){
         Path userRootPath = Paths.get(getJarPath().toString(), FileKey.FILEROOT,FileKey.IMAGESTORAGE,userName);
-        log.info(userRootPath.toString());
+//        log.info(userRootPath.toString());
         if(!Files.exists(userRootPath,new LinkOption[]{ LinkOption.NOFOLLOW_LINKS})){
             try {
                 Path path = Files.createDirectories(userRootPath);
--- src/main/java/moe/keshane/PikaImage/Web/Controller/DirectoryController.java
+++ src/main/java/moe/keshane/PikaImage/Web/Controller/DirectoryController.java
@@ -3,6 +3,7 @@ package moe.keshane.PikaImage.Web.Controller;
 import lombok.extern.slf4j.Slf4j;
 import moe.keshane.PikaImage.Common.FileKey;
 import moe.keshane.PikaImage.Common.KeySet;
+import moe.keshane.PikaImage.Exception.PageNotFoundException;
 import moe.keshane.PikaImage.Util.FileUtils;
 import moe.keshane.PikaImage.Util.SessionUtils;
 import moe.keshane.PikaImage.Util.StringUtils;
@@ -10,9 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -42,7 +46,7 @@ public class DirectoryController {
     }
 
     @RequestMapping("/list/**")
-    public String list(HttpServletRequest request, HttpSession session, ModelMap modelMap) {
+    public String list(HttpServletRequest request, HttpSession session, ModelMap modelMap, @RequestParam(value = "message")String message) {
         String uri = null;
         try {
             uri = URLDecoder.decode(request.getRequestURI(),"UTF-8");
@@ -50,16 +54,16 @@ public class DirectoryController {
             e.printStackTrace();
         }
         String uriP = uri.substring(uri.indexOf("list") + 4, uri.length());
-        log.info("访问的路径去除list为: {}",uriP);
+//        log.info("访问的路径去除list为: {}",uriP);
         String username = SessionUtils.getUserNameFromSession(session);
         String uriPath = FileUtils.getPathByUserNameAndUri(username, uriP);
-        log.info("组装好之后的文件路径为: {}",uriPath);
+//        log.info("组装好之后的文件路径为: {}",uriPath);
         if (!FileUtils.isExist(uriPath)) {
 //            FileUtils.createDirFromPath(uriPath);
             modelMap.put(KeySet.USERNAME, username);
             modelMap.put(KeySet.MESSAGE_TYPE, KeySet.DANGER);
             modelMap.put(KeySet.MESSAGE, "目录不存在");
-            return "list";
+            throw new PageNotFoundException("目录不存在");
         }
         List<String> dirList = FileUtils.listDir(uriPath);
         List<String> fileList = FileUtils.listFile(uriPath);
@@ -71,8 +75,7 @@ public class DirectoryController {
     }
 
     @RequestMapping(value = "/directory",method = RequestMethod.POST)
-    @ResponseBody
-    public String createDirectory(String sourceDir,String targetDirName, HttpServletRequest request, HttpSession session) {
+    public String createDirectory(String sourceDir, String targetDirName, HttpSession session, RedirectAttributes redirectAttributes) {
         if(StringUtils.isHasAny(targetDirName,".","/","|","\\",",","`","~","*","^","%","#")){
             return "创建目录失败，包含违规名";
         }
@@ -81,9 +84,25 @@ public class DirectoryController {
         String uriPath = FileUtils.getPathByUserNameAndUri(username, uriP);
         boolean b = FileUtils.createDirFromPath(Paths.get(uriPath,targetDirName).toString());
         if(b){
-            return "创建目录成功";
+            return "redirect:"+sourceDir;
         }
         return "创建目录失败，该目录已经存在";
+    }
+
+    @RequestMapping(value = "/deletedirectory",method = RequestMethod.POST)
+    @ResponseBody
+    public String deleteDirectory(String sourceDir,String targetDirName, HttpSession session, RedirectAttributes redirectAttributes) {
+        if(StringUtils.isHasAny(targetDirName,".","/","|","\\",",","`","~","*","^","%","#")){
+            return "创建目录失败，包含违规名";
+        }
+        String uriP = sourceDir.substring(sourceDir.indexOf("list") + 4, sourceDir.length());
+        String username = SessionUtils.getUserNameFromSession(session);
+        String uriPath = FileUtils.getPathByUserNameAndUri(username, uriP);
+        boolean b = FileUtils.deleteDirectory(Paths.get(uriPath,targetDirName).toString());
+        if(b){
+            return "删除目录成功";
+        }
+        return "删除目录失败";
     }
 
 }
