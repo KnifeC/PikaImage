8f1bf857a8b53c1121792bcea9ccc48d97692034
KnifeC <keshanechen@gmail.com>
2019-11-22 20:51:01 +0800

    add message for login and register

--- src/main/java/moe/keshane/PikaImage/Web/Controller/UserController.java
+++ src/main/java/moe/keshane/PikaImage/Web/Controller/UserController.java
@@ -7,10 +7,12 @@ import moe.keshane.PikaImage.Service.UserService;
 import moe.keshane.PikaImage.Util.MessageUtils;
 import moe.keshane.PikaImage.Util.StringUtils;
 import moe.keshane.PikaImage.Util.UserCookieUtils;
+import moe.keshane.PikaImage.Web.Result.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -23,7 +25,7 @@ public class UserController {
     UserService userService;
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String register(String username, String password, String rePassword, ModelMap modelMap) {
+    public String register(String username, String password, String rePassword, ModelMap modelMap,RedirectAttributes redirectAttributes) {
         log.info("Register username : {} password : {} repassword : {}",username,password,rePassword);
         if (StringUtils.isNull(username, password, rePassword)) {
             MessageUtils.sendDangerMessage(modelMap,"输入数据不能为空");
@@ -34,11 +36,12 @@ public class UserController {
             return "register";
         }
         User register = userService.register(username, password, rePassword);
+        redirectAttributes.addFlashAttribute(KeySet.MESSAGE,new Message("注册成功",KeySet.SUCCESS));
         return "redirect:/login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(String username, String password, HttpServletResponse response, ModelMap modelMap) {
+    public String login(String username, String password, HttpServletResponse response, ModelMap modelMap,RedirectAttributes redirectAttributes) {
         if (StringUtils.isNull(username, password)) {
             MessageUtils.sendDangerMessage(modelMap,"输入数据不能为空");
             return "login";
@@ -46,6 +49,7 @@ public class UserController {
         User user = userService.login(username, password);
         UserCookieUtils.setUserIdCookie(response, user.getUserId());
         modelMap.put(KeySet.USERNAME, user.getUsername());
+        redirectAttributes.addFlashAttribute(KeySet.MESSAGE,new Message("登陆成功",KeySet.SUCCESS));
         return "redirect:/";
 
     }
